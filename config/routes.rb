04a45f3309f54597d54d48828e39d1ee64fcaaa4
bear22309 <150596708+bear22309@users.noreply.github.com>
2024-08-14 14:18:12 -0400

    Resolve merge conflicts in tasks_controller, application_controller, index.js, and package.json

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,1 @@
-Rails.application.routes.draw do
-  root 'static_pages#index'
 
-  namespace :api do
-    resources :users, only: [:create]
-    resources :tasks, only: [:index, :create, :show, :update, :destroy] do
-      member do
-        put 'mark_complete'
-        put 'mark_active'
-      end
-    end
-  end
-
-  # Catch-all route for unknown paths, redirect to root
-  get '*path', to: redirect('/')
-end
